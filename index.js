cb8d6d18d6534af2ef8fc777677104405345a704
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2023-08-08 22:06:41 +0300

    submitting buttons done

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const thanksContainer = document.querySelector(".thank-you")
 const submitBtn = document.getElementById("submit-btn")
 
 const rateAgainBtn = document.getElementById("rate-again-btn")
+
+submitBtn.addEventListener("click", function() {
+    thanksContainer.classList.remove("hidden")
+    mainContainer.classList.add("hidden")
+;})
+
+rateAgainBtn.addEventListener("click", function() {
+    thanksContainer.classList.add("hidden")
+    mainContainer.classList.remove("hidden")
+;})
