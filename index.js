df472fdcdb7ea422a12eda19d3b7d9a507dfc5ef
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2023-08-08 21:57:54 +0300

    buttons added to JS

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const mainContainer = document.querySelector(".main-container")
 
 const thanksContainer = document.querySelector(".thank-you")
+
+const submitBtn = document.getElementById("submit-btn")
+
+const rateAgainBtn = document.getElementById("rate-again-btn")
